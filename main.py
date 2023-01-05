114cd72b69825f0e73acf2a671d018b1741b07b3
Pandega27 <122007293+Pandega27@users.noreply.github.com>
2023-01-05 14:50:02 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Hallo Im Pandega Bimata Abhinaya (AB)')
+    print('27 November 2003')
+    print('Im 19 Y.o ')
+    print('i am from Indonesia')
+    print('i live in vietnam for 3 years')
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
